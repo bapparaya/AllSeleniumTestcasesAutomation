75c821f8614f0b5e5ae11b106ce0593fa29b7b6a
bapparaya <bryan.w@kanrad.com>
2025-02-18 09:27:29 +0530

    file upload

---
+++ src/main/java/com/qa/automationtesting/pages/Miscaleniuospage.java
@@ -0,0 +1,34 @@
+package com.qa.automationtesting.pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.qa.automationpractice.base.BaseClass;
+
+public class Miscaleniuospage extends BaseClass {
+
+
+@FindBy(id="singleFileInput")
+WebElement singlefileuploadbtn;
+
+public Miscaleniuospage(){
+	super();
+	PageFactory.initElements(driver, this);
+}
+
+
+public String singleFileUpload() {
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+	js.executeScript("arguments[0].scrollIntoView(true)", singlefileuploadbtn );
+	//singlefileuploadbtn.click();
+	singlefileuploadbtn.sendKeys("C:\\Users\\bapparayar\\Downloads\\Bapparaya 5yr automation (1).pdf");
+	return singlefileuploadbtn.getAttribute("value");
+}
+
+
+
+
+
+}
---
+++ src/test/java/com/qa/automationtesting/testcases/Miscaleniuostest.java
@@ -0,0 +1,39 @@
+package com.qa.automationtesting.testcases;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.qa.automationpractice.base.BaseClass;
+import com.qa.automationtesting.pages.Miscaleniuospage;
+
+public class Miscaleniuostest extends BaseClass{
+
+	Miscaleniuospage mp;
+
+@BeforeTest
+@Parameters("browser")
+void setup(String browser) {
+	initialize(browser);
+	 mp = new Miscaleniuospage();
+}
+
+@Test
+void verifySingleFileUpload() {
+	String filename = mp.singleFileUpload();
+	Assert.assertEquals(filename, "C:\\fakepath\\Bapparaya 5yr automation (1).pdf");
+}
+@AfterTest
+void tearDown() {
+	
+}
+
+
+
+
+
+}
